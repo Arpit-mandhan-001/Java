198baebc54b02033dc755a7d6b073ceb8c95d63f
Arpit-mandhan-001 <arpitmandhan001@gmail.com>
2026-04-02 23:48:24 +0530

    String 02

--- 02_string/Main.java
+++ 02_string/Main.java
@@ -5,5 +5,21 @@ public class Main {
          st.address = "Rohini, Delhi";
          st.rollNumber = 19;
          System.out.println(st.name + st.address + st.rollNumber + " " + st.standard);
+
+         // Heap mai String pool krke chizz hoti hai joh string pool mai ek br aajata hai usse wapis se store nhi krte sidha yahhi se reference dete hai
+         String x = "RAM";
+         String a = new String(x); // ye ek object create krta hai usme value daalta hai aur variable mai uska reference store hota hai
+         String b = new String(x);
+         String c = "RAM";
+         String d = "RAM";
+
+         System.out.println(a == b);
+         System.out.println(c == d);
+
+         System.out.println("this is c " + c);
+         System.out.println("this is d " + d);
+         c = "XYZ"; 
+         System.out.println("this is c " + c);
+         System.out.println("this is d " + d);
     } 
 }
